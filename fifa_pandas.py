11146e9c2b0447a7a238fcd8cf722f59f9a44732
rudyabs <rudy.abs@live.com>
2019-05-20 23:43:10 +0700

    v0.0.1 - 20/05/2019

---
+++ fifa_pandas.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+df_main = pd.read_csv('data.csv')
+# print(df['Name'])
+
+df = df_main.copy()
+
+young_good = df[df['Age']<25][df['Overall']>=85]
+young_bad = df[df['Age']<25][df['Overall']<85]
+old_good = df[df['Age']>=25][df['Overall']>=85]
+old_bad = df[df['Age']>=25][df['Overall']<85]
+
+plt.scatter(young_good['Age'], young_good['Overall'], c='red')
+plt.scatter(young_bad['Age'], young_bad['Overall'], c='blue')
+plt.scatter(old_good['Age'], old_good['Overall'], c='yellow')
+plt.scatter(old_bad['Age'], old_bad['Overall'], c='green')
+
+plt.show()
