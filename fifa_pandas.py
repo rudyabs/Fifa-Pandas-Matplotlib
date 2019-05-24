dc8b88fc8d0962a1b99d455db303c2e8081cc5cc
rudyabs <rudy.abs@live.com>
2019-05-24 21:14:45 +0700

    v0.0.2 - 20/05/2019 - Change Variable

--- fifa_pandas.py
+++ fifa_pandas.py
@@ -7,14 +7,16 @@ df_main = pd.read_csv('data.csv')
 
 df = df_main.copy()
 
-young_good = df[df['Age']<25][df['Overall']>=85]
-young_bad = df[df['Age']<25][df['Overall']<85]
-old_good = df[df['Age']>=25][df['Overall']>=85]
-old_bad = df[df['Age']>=25][df['Overall']<85]
+young_good = df[(df['Age']<25) & (df['Overall']>=85)]
+young_bad = df[(df['Age']<25) & (df['Overall']<85)]
+old_good = df[(df['Age']>=25) & (df['Overall']>=85)]
+old_bad = df[(df['Age']>=25) & (df['Overall']<85)]
 
 plt.scatter(young_good['Age'], young_good['Overall'], c='red')
 plt.scatter(young_bad['Age'], young_bad['Overall'], c='blue')
 plt.scatter(old_good['Age'], old_good['Overall'], c='yellow')
 plt.scatter(old_bad['Age'], old_bad['Overall'], c='green')
+plt.grid(True)
+plt.legend(['Young_Good', 'Young_Bad', 'Old_Good','Old_Bad'])
 
 plt.show()
